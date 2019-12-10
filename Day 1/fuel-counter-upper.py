cf6d26bc6b367391c42378a603353b864d88fc41
Donald N <dolnguyen@ucdavis.edu>
2019-12-09 21:36:59 -0800

    Git issues

--- Day 1/fuel-counter-upper.py
+++ Day 1/fuel-counter-upper.py
@@ -1,9 +1,31 @@
 import math
 
-module_mass_input_file = open("module-mass-input.txt", "r")
+def fuel_calculator(mass):
+    return math.floor(int(mass) / 3) - 2
 
-sum = 0
-for module_mass in module_mass_input_file:
-    sum += math.floor(int(module_mass) / 3) - 2
+# Part 1
 
-print(sum)
+def main():
+    module_mass_input_file = open("module-mass-input.txt", "r")
+
+    sum = 0
+    for module_mass in module_mass_input_file:
+        sum += fuel_calculator(module_mass)
+
+    print(sum)
+
+    # Part 2
+
+    module_mass_input_file.seek(0)
+
+    sum = 0
+    for module_mass in module_mass_input_file:
+        fuel = fuel_calculator(module_mass)
+        while(fuel > 0):
+            sum += fuel
+            fuel = fuel_calculator(fuel)
+
+    print(sum)
+
+if __name__ == "__main__":
+    main()
